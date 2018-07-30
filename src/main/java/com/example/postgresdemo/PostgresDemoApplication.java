745a08fe87b9d8364e0a2f6aa53ac477cc0198c4
German Muñoz <germanmr@amr.org.ar>
2018-07-30 14:16:39 -0300

    Ahora tambien en disponible en tomcat!!

--- src/main/java/com/example/postgresdemo/PostgresDemoApplication.java
+++ src/main/java/com/example/postgresdemo/PostgresDemoApplication.java
@@ -2,23 +2,17 @@ package com.example.postgresdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.client.RestTemplate;
 
 import com.example.postgresdemo.controller.ClienteRest;
 
-// scanBasePackages={"com.example.something", "com.example.application"}
-
-@SpringBootApplication(scanBasePackages = {"com.example.postgresdemo"})
-@ComponentScan({"com.example.postgresdemo.controller"})
-@EntityScan("com.example.postgresdemo")
-// @EnableJpaRepositories("com.example.postgresdemo")
+@SpringBootApplication
 @EnableJpaAuditing
-public class PostgresDemoApplication {
+public class PostgresDemoApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(PostgresDemoApplication.class, args);
